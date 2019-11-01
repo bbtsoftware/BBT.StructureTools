9a9cfb1f3795978f72acefe7263fb3e530b515f1
Marco Bertschi <ber@bbtsoftware.ch>
2019-11-01 15:40:34 +0100

    enhanced doc

--- src/BBT.StructureTools/Strategy/ICreateByBaseAsCriterionStrategy.cs
+++ src/BBT.StructureTools/Strategy/ICreateByBaseAsCriterionStrategy.cs
@@ -3,8 +3,10 @@
     using BBT.StrategyPattern;
 
     /// <summary>
-    /// Definition of a strategy where a specific implementation instance is created based on a
-    /// specific implementation it is depending on.
+    /// Definition of a strategy where a specific implementation of the target is instantiated based
+    /// on the specific source's type. This is intended to be used where a source and a target property
+    /// may have multiple specific implementations inheriting from a base class or interface which isn't
+    /// the same base for source and target.
     /// </summary>
     /// <typeparam name="TBaseInterface">Base interface type (on which <typeparamref name="TInterface"/> depends on.</typeparam>
     /// <typeparam name="TInterface">Base interface covering the instance which is created depending on <typeparamref name="TBaseInterface"/>.</typeparam>
